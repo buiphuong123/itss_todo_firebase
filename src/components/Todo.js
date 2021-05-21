29a6b3f5274b6dd445473a9daba17793ccf547f5
unknown <buiphuong>
2021-05-21 15:22:23 +0700

    save todo on firebase

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,13 +6,13 @@ import Input from './Input';
 import Filter from './Filter';
 
 /* カスタムフック */
-import useStorage from '../hooks/storage';
+import useFbStorage from '../hooks/fbStorage';
 
 /* ライブラリ */
 import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems, clearItems] = useStorage();
+  const [items, addItem, updateItem, clearItems] = useFbStorage();
   
   const [filter, setFilter] = React.useState('ALL');
 
@@ -23,23 +23,17 @@ function Todo() {
   });
   
   const handleCheck = checked => {
-    const newItems = items.map(item => {
-      if (item.key === checked.key) {
-        item.done = !item.done;
-      }
-      return item;
-    });
-    putItems(newItems);
+    updateItem(checked);
   };
   
   const handleAdd = text => {
-    putItems([...items, { key: getKey(), text, done: false }]);
+    addItem({ text, done: false });
   };
   
   const handleFilterChange = value => setFilter(value);
 
   return (
-    <article class="panel is-danger">
+    <div className="panel panel-success">
       <div className="panel-heading">
         <span class="icon-text">
           <span class="icon">
@@ -55,7 +49,7 @@ function Todo() {
       />
       {displayItems.map(item => (
         <TodoItem 
-          key={item.key}
+          key={item.id}
           item={item}
           onCheck={handleCheck}
         />
@@ -68,7 +62,7 @@ function Todo() {
           全てのToDoを削除
         </button>
       </div>
-    </article>
+    </div>
   );
 }
 
